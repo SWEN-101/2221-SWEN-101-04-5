ae95722dbcb8f5a24fc5e7af095677087dcb9901
sleepyrakun <ah6931@g.rit.edu>
2022-11-15 18:59:58 -0500

    weather progress
    Added date that changes, now needs to fix formatting within the box.

---
+++ scripts/weather_script.js
@@ -0,0 +1,16 @@
+function getDate(){
+    var return_date = document.getElementById("date"); // Set return date variable.
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //Today!
+    const today = new Date();
+    const yyyy = today.getFullYear();
+                
+    let month = months[today.getMonth()]; 
+    let day = today.getDate();
+    let weekday = days[today.getDay()];
+
+    const formattedToday = weekday + " " + month + ' ' + day + ', ' + yyyy;
+
+    return_date.innerHTML = formattedToday;
+}
